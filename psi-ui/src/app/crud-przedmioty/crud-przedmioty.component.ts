499ce5a219f59750b9d1f0762aa78a0e65a96f66
Kinga Buczkowska <238465@student.pwr.edu.pl>
2020-11-14 19:05:40 +0100

    buttons added

--- psi-ui/src/app/crud-przedmioty/crud-przedmioty.component.ts
+++ psi-ui/src/app/crud-przedmioty/crud-przedmioty.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { HistoryComponent } from '../shared/history/history.component';
 
 @Component({
   selector: 'app-crud-przedmioty',
@@ -7,12 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrudPrzedmiotyComponent implements OnInit {
 
-  constructor() { }
-
   subjectFields = ['Kod', 'Nazwa polska', 'Nazwa angielska', 'Typ', 'Rodzaj', 'Język zajęć']
 
   classFields = ['Typ', 'Czas na uczelni', 'Czas w domu', 'Sposób zaliczenia', 'ECTS', 'Kończący', 'Badawczy']
+
+  isDetails = false;
+  
+  constructor(public dialog: MatDialog) { }
+
   ngOnInit(): void {
+  }
+
+  toggleDetails() {
+    this.isDetails = !this.isDetails;
+  }
+
+  openDialog(): void {
+    this.dialog.open(HistoryComponent, {
+      height: '500px',
+      width: '400px'
+    });
   }
 
 }

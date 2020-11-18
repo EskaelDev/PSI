ee32f2cce514697b9d9201195932e4df6b5897e6
Kinga Buczkowska <238465@student.pwr.edu.pl>
2020-11-18 20:18:31 +0100

    LO fix

--- psi-ui/src/app/efekty/crud-efekty/crud-efekty.component.ts
+++ psi-ui/src/app/efekty/crud-efekty/crud-efekty.component.ts
@@ -5,6 +5,7 @@ import { HistoryComponent } from '../../shared/history/history.component';
 
 export interface LearningOutcome {
   symbol: string;
+  type: string;
   description: string;
   universal: string;
   level6: string;
@@ -12,9 +13,9 @@ export interface LearningOutcome {
 }
 
 const LEARING_OUTCOMES: LearningOutcome[] = [
-  {symbol: 'KINF_W01', description: 'Posiada podstawową wiedzę ogólną z zakresu wybranych gałęzi matematyki: analizy matematycznej, algebry liniowej i geometrii analitycznej, logiki matematycznej, matematyki dyskretnej oraz rachunku prawdopodobieństwa i statystyki matematycznej, tworzącą podstawy teoretyczne konieczne do rozwiązywania informatycznych problemów inżynierskich', universal: 'P6U_W', level6: 'P6S_WG', level6eng: ''},
-  {symbol: 'KINF_W03', description: 'Ma podstawową wiedzę w zakresie wybranych działów fizyki', universal: 'P6U_W', level6: 'P6S_WG', level6eng: ''},
-  {symbol: 'KINF_W04', description: 'Zna i rozumie podstawowe struktury danych, algorytmy oraz konstrukcje programistyczne w różnych językach programowania', universal: 'P6U_W', level6: 'P6S_WG', level6eng: 'P6S_WG_inż'},
+  {symbol: 'KINF_W01', type: 'W', description: 'Posiada podstawową wiedzę ogólną z zakresu wybranych gałęzi matematyki: analizy matematycznej, algebry liniowej i geometrii analitycznej, logiki matematycznej, matematyki dyskretnej oraz rachunku prawdopodobieństwa i statystyki matematycznej, tworzącą podstawy teoretyczne konieczne do rozwiązywania informatycznych problemów inżynierskich', universal: 'P6U_W', level6: 'P6S_WG', level6eng: ''},
+  {symbol: 'KINF_W03', type: 'U', description: 'Ma podstawową wiedzę w zakresie wybranych działów fizyki', universal: 'P6U_W', level6: 'P6S_WG', level6eng: ''},
+  {symbol: 'KINF_W04', type: 'K', description: 'Zna i rozumie podstawowe struktury danych, algorytmy oraz konstrukcje programistyczne w różnych językach programowania', universal: 'P6U_W', level6: 'P6S_WG', level6eng: 'P6S_WG_inż'},
 ];
 
 
@@ -25,7 +26,7 @@ const LEARING_OUTCOMES: LearningOutcome[] = [
 })
 export class CrudEfektyComponent implements OnInit {
 
-  displayedColumns: string[] = ['symbol', 'description', 'universal', 'level6', 'level6eng', 'actions'];
+  displayedColumns: string[] = ['symbol', 'type', 'description', 'universal', 'level6', 'level6eng', 'actions'];
   dataSource = LEARING_OUTCOMES;
 
   isDetails = false;

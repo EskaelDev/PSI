8a6fa317ab418e14436a5935d7c59221367facb5
Tomek <rainbowvme@gmail.com>
2020-12-19 12:36:20 +0100

    DB context

--- src/backend/SyllabusManager.Data/SyllabusManagerDbContext.cs
+++ src/backend/SyllabusManager.Data/SyllabusManagerDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using SyllabusManager.Data.Models.FieldOfStudies;
 using SyllabusManager.Data.Models.LearningOutcomes;
+using SyllabusManager.Data.Models.ManyToMany;
 using SyllabusManager.Data.Models.Subjects;
 using SyllabusManager.Data.Models.Syllabuses;
 using SyllabusManager.Data.Models.User;
@@ -15,6 +16,26 @@ namespace SyllabusManager.Data
         public DbSet<Syllabus> Syllabuses { get; set; }
         public DbSet<Subject> Subjects { get; set; }
 
+        //public DbSet<SubjectTeacher> SubjectTeachers { get; set; }
+
         public SyllabusManagerDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<SubjectTeacher>().HasKey(sc => new { sc.SubjectId, sc.TeacherId });
+
+            modelBuilder.Entity<SubjectTeacher>()
+                .HasOne(st => st.Subject)
+                .WithMany(s => s.SubjectsTeachers)
+                .HasForeignKey(st => st.SubjectId);
+
+
+            modelBuilder.Entity<SubjectTeacher>()
+                .HasOne(st => st.Teacher)
+                .WithMany(u => u.SubjectsTeachers)
+                .HasForeignKey(st => st.TeacherId);
+        }
     }
 }

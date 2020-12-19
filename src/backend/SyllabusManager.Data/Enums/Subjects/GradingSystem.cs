f07ef7c61b5ce0d6ea9333082d188ebe8cc7e7c6
Tomek <rainbowvme@gmail.com>
2020-12-19 12:35:10 +0100

    Grading system enum

---
+++ src/backend/SyllabusManager.Data/Enums/Subjects/GradingSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SyllabusManager.Data.Enums.Subjects
+{
+    /// <summary>
+    /// Oceny (F – formująca (w trakcie semestru), P – podsumowująca (na koniec semestru)
+    /// </summary>
+    public enum GradingSystem
+    {
+        DuringSemester,
+        Final
+    }
+
+}

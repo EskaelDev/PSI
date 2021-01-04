befc071cae4272c6af8741233140cde53e2c4f2c
Kinga Buczkowska <238465@student.pwr.edu.pl>
2021-01-04 18:56:37 +0100

    Model changes

--- src/frontend/src/app/core/models/field-of-study/specialization.ts
+++ src/frontend/src/app/core/models/field-of-study/specialization.ts
@@ -1,7 +1,5 @@
 import { NonVersionedModelBase } from "../non-versioned-model-base";
-import { FieldOfStudy } from "./field-of-study";
 
 export class Specialization extends NonVersionedModelBase {
     name: string = '';
-    fieldOfStudy: FieldOfStudy | null = null;
 }

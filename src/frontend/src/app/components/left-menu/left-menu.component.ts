1a8c6a47d66ff4804f2552c6b2b65cc6444d64d5
Kinga Buczkowska <238465@student.pwr.edu.pl>
2021-01-17 13:00:57 +0100

    Home and navbar icons

--- src/frontend/src/app/components/left-menu/left-menu.component.ts
+++ src/frontend/src/app/components/left-menu/left-menu.component.ts
@@ -1,8 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {onSideNavChange, animateText} from '../../animations/animations';
-import {MatSidenav} from '@angular/material/sidenav';
-import {NavbarService} from '../../services/navbar/navbar.service';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { onSideNavChange, animateText } from '../../animations/animations';
+import { MatSidenav } from '@angular/material/sidenav';
+import { NavbarService } from '../../services/navbar/navbar.service';
+import { NgxPermissionsService } from 'ngx-permissions';
 
 interface Page {
   link: string;
@@ -14,7 +14,7 @@ interface Page {
   selector: 'app-left-menu',
   templateUrl: './left-menu.component.html',
   styleUrls: ['./left-menu.component.scss'],
-  animations: [onSideNavChange, animateText]
+  animations: [onSideNavChange, animateText],
 })
 export class LeftMenuComponent implements OnInit {
   @Input() sidenav: MatSidenav | undefined;
@@ -22,22 +22,22 @@ export class LeftMenuComponent implements OnInit {
   public sideNavState = false;
   public linkText = false;
 
-  public pages: Page[] = [
-    {name: 'Strona główna', link: '/home', icon: 'home'},
-    {name: 'Dokumenty', link: '/documents', icon: 'get_app'},
-    {name: 'Programy studiów', link: '/syllabus', icon: 'filter_frames'},
-    {name: 'Przedmioty', link: '/subject', icon: 'insert_drive_file'},
-    {name: 'Efekty uczenia się', link: '/learning-outcome/choose', icon: 'school'},
-    {name: 'Administracja', link: '/syllabus/acceptance', icon: 'settings'}
+  public possiblePages: Page[] = [
+    { name: 'Strona główna', link: '/home', icon: 'home' },
+    { name: 'Dokumenty', link: '/documents', icon: 'get_app' },
   ];
 
   public userNavigation: Page[] = [
-    {name: '', link: '/account', icon: 'perm_identity'},
-    {name: '', link: '/logout', icon: 'power_settings_new'},
+    { name: '', link: '/account', icon: 'perm_identity' },
+    { name: '', link: '/logout', icon: 'power_settings_new' },
   ];
 
-  constructor(private sidenavService: NavbarService) {
-  }
+  public pages: Page[] = [];
+
+  constructor(
+    private sidenavService: NavbarService,
+    private readonly permissions: NgxPermissionsService
+  ) {}
 
   onSidenavToggle(): void {
     this.sideNavState = !this.sideNavState;
@@ -49,6 +49,57 @@ export class LeftMenuComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.loadActionButtons();
   }
 
+  loadActionButtons() {
+    this.permissions.permissions$.subscribe((p) => {
+      this.pages = [];
+      this.possiblePages.forEach((p) => {
+        this.pages.push(p);
+      });
+
+      this.permissions.hasPermission(['Admin', 'Teacher']).then((res) => {
+        if (res) {
+          this.pages.push({
+            name: 'Programy studiów',
+            link: '/subject/choose',
+            icon: 'filter_frames',
+          });
+          this.pages.push({
+            name: 'Przedmioty',
+            link: '/subject/choose',
+            icon: 'insert_drive_file',
+          });
+          this.pages.push({
+            name: 'Efekty uczenia się',
+            link: '/learning-outcome/choose',
+            icon: 'school',
+          });
+        }
+      });
+
+      this.permissions
+        .hasPermission(['Admin', 'StudentGovernment'])
+        .then((res) => {
+          if (res) {
+            this.pages.push({
+              name: 'Akceptacja',
+              link: '/syllabus/acceptance',
+              icon: 'done',
+            });
+          }
+        });
+
+      this.permissions.hasPermission('Admin').then((res) => {
+        if (res) {
+          this.pages.push({
+            name: 'Administracja',
+            link: '/admin/menu',
+            icon: 'settings',
+          });
+        }
+      });
+    });
+  }
 }
